5de48929e8646ca6ed7929b29107cdffa3db6201
jordanthesier <jordanthesier@gmail.com>
2025-04-28 15:22:02 -0400

    Updating 06_Ordination analysis without outlier

--- code/colors.R
+++ code/colors.R
@@ -3,3 +3,30 @@ AgeRange_colors <- c(
   "JUVENILE" = "dodgerblue4",
   "SUB-ADULT" = "#A0E0BA",
   "ADULT" = "#D9CC3C")
+
+# Set the phylum colors
+phylum_colors <- c(
+  Acidobacteriota = "navy", 
+  Actinomycetota = "darkslategray2", 
+  Armatimonadota = "deeppink1",
+  Alphaproteobacteria = "plum2", 
+  Bacteroidota = "gold", 
+  Betaproteobacteria = "plum1", 
+  Bdellovibrionota = "red1",
+  Chloroflexi="black", 
+  "Candidatus Kapabacteria" = "firebrick",
+  Cyanobacteriota = "limegreen",
+  Deltaproteobacteria = "grey", 
+  Desulfobacterota="magenta",
+  Firmicutes = "#3E9B96",
+  Gammaproteobacteria = "greenyellow",
+  "Marinimicrobia (SAR406 clade)" = "yellow",
+  Myxococcota = "#B5D6AA",
+  Pseudomonadota = "royalblue",
+  Planctomycetota = "darkorange", 
+  "SAR324 clade(Marine group B)" = "olivedrab",
+  Proteobacteria_unclassified = "greenyellow",
+  Thermoplasmatota = "green",
+  Thermoproteota = "palevioletred1",
+  Verrucomicrobiota = "darkorchid1",
+  Other = "grey")
